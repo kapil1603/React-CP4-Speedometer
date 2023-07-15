285c250ff76ae9b7e5f3d9c3180b8d2912047cd2
kapil1603 <kapilkumar1603@gmail.com>
2023-07-15 18:51:57 +0000

    React-CP4-Speedometer

--- src/components/Speedometer/index.js
+++ src/components/Speedometer/index.js
@@ -1,5 +1,5 @@
 // Write your code here
-import Component from 'react'
+import {Component} from 'react'
 import './index.css'
 
 class Speedometer extends Component {
@@ -17,17 +17,32 @@ class Speedometer extends Component {
     const {count} = this.state
     return (
       <div className="Container">
-        <h1 className="heading">
-          Speed is <span className="count">{count}</span> mph
-        </h1>
+        <h1 className="heading">SPEEDOMETER</h1>
+        <div>
+          {' '}
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/speedometer-img.png"
+            alt="speedometer"
+            className="image1"
+          />{' '}
+        </div>
+        <h1 className="heading">Speed is {count}mph</h1>
         <p className="para">Min limit is 0mph, Max Limit is 200mph</p>
 
         <div>
-          <button type="button" className="button" onClick={this.increaseSpeed}>
+          <button
+            type="button"
+            className="button blue-color"
+            onClick={this.increaseSpeed}
+          >
             Accelerate
           </button>
-          <button type="button" className="button" onClick={this.decreaseSpeed}>
-            Accelerate
+          <button
+            type="button"
+            className="button white-color"
+            onClick={this.decreaseSpeed}
+          >
+            Break
           </button>
         </div>
       </div>
